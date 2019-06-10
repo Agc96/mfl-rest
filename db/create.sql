a684c1abbd9d8cd551d5aa3dac6ee979d2a2cb9e
Agc96 <anthony.gutierrez@pucp.pe>
2019-06-10 17:17:07 -0500

    2019-06-10 Evitar que se suban archivos con información sensible

--- db/create.sql
+++ db/create.sql
@@ -1,8 +1,13 @@
+-- Primero, crear el usuario webapp (ver webapp.sql)
+
+-- Luego, crear la base de datos
 CREATE DATABASE myfirstlogin;
 
+-- Otorgar permisos al administrador de BD y a la aplicación REST
 GRANT ALL ON DATABASE myfirstlogin TO postgres;
 GRANT CONNECT ON DATABASE myfirstlogin TO webapp;
 
+-- Crear tablas
 CREATE TABLE mfl_user (
     user_id SERIAL NOT NULL PRIMARY KEY,
     username VARCHAR(64) NOT NULL,
@@ -11,8 +16,11 @@ CREATE TABLE mfl_user (
     email VARCHAR(128) NOT NULL
 );
 
+-- Otorgar permisos a la aplicación REST
 GRANT USAGE ON SCHEMA public TO webapp;
 GRANT SELECT ON ALL TABLES IN SCHEMA public TO webapp;
 GRANT INSERT ON ALL TABLES IN SCHEMA public TO webapp;
 GRANT UPDATE ON ALL TABLES IN SCHEMA public TO webapp;
 GRANT DELETE ON ALL TABLES IN SCHEMA public TO webapp;
+
+-- Por último, insertar los datos iniciales (ver el resto de archivos SQL)
---
+++ db/webapp.sql
@@ -0,0 +1,2 @@
+-- Reemplazar 'pass' con la contraseña en texto plano
+CREATE USER webapp WITH ENCRYPTED PASSWORD 'pass';
